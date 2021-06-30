89c94933323ef47f2dd9de00b5dd1d21f9575e61
BorkStick <4ciiDicBa55@gmail.com>
2021-06-30 11:44:53 -0500

    day 7 classwork

---
+++ html_basics/Day_7-js/Classwork/main.js
@@ -0,0 +1,17 @@
+console.log("hello world");
+
+let bork = "let test";
+
+console.log(bork);
+
+let borkTest = "this is a test of the emergency thingamabob";
+
+console.log(borkTest + bork);
+
+let z = 6;
+
+console.log("the value z is " + z);
+
+let foo = null;
+
+console.log(foo);
