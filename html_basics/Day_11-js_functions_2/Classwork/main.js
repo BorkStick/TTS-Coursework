9ff694c46b7e959d3c73a4a0c738c68ee1e42c31
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-07-08 08:20:34 -0500

    day 11

--- html_basics/Day_11-js_functions_2/Classwork/main.js
+++ html_basics/Day_11-js_functions_2/Classwork/main.js
@@ -1,2 +1,13 @@
 console.log("test");
 
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+function doMath(operation, number1, number2) {
+    return operation(number1, number2)
+}
+
+let sum = doMath(add, 1, 2);
+console.log(sum);
+
