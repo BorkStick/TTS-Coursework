bbc010c9165600e557455418b9a55cae321a32e2
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-07-08 08:14:28 -0500

    week 3 day 10 start

--- html_basics/Day_10-js_functions_1/Classwork/Gratuity/main.js
+++ html_basics/Day_10-js_functions_1/Classwork/Gratuity/main.js
@@ -1,11 +1,11 @@
-let billAmount = 1;
+let billAmount = 100;
 
 
 // get bill total from form input
 function enterBill() {
     let billAmount = document.getElementById("billAmountForm").value;
     document.getElementById("bill-amount").innerHTML = billAmount;
-    console.log("bill amount: " + billAmount);
+    //console.log("bill amount: " + billAmount);
 }
 
 console.log("bill amount: " + billAmount);
@@ -30,15 +30,14 @@ console.log("Your total including gratuity is:" + `totalWithGrat(billAmount)`);
 
 // add stuff to the frontend
 
+// Bill 
 const billTotal = document.getElementById("bill-amount");
 billTotal.innerHTML = billAmount;
 
-
+// gratuity 
 const gratuityAmount = document.getElementById("gratuity");
 gratuityAmount.innerHTML = gratuity();
 
-
-
-
+//total 
 const total = document.getElementById("total");
 total.innerHTML = totalWithGrat(billAmount);
