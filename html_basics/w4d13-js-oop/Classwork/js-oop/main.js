0409f47bf53bc39a6380a76869e5eb895cf5e51b
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-07-13 11:34:24 -0500

    w4d13 class end

--- html_basics/w4d13-js-oop/Classwork/js-oop/main.js
+++ html_basics/w4d13-js-oop/Classwork/js-oop/main.js
@@ -1,18 +1,45 @@
 class Fruit {
+    fruitName = "fruit";
+    howNutritious = "unknown";
     constructor(name) {
-        this.name = name;
+        this.fruitName = name;
     }
+
+    changeName = function (newName) {
+        this.fruitName = newName;
+    };
+
     speak = function () {
-        console.log("I am a " + this.name);
-    }
+        console.log("Hey my name is " + this.fruitName);
+    };
+
+    calculateNutrition = function () {
+        if (this.fruitName === "Apple") {
+            this.howNutritious = "very";
+        } else if (this.fruitName === "Banana") {
+            this.howNutritious = "pretty";
+        } else {
+            this.howNutritious = "not very";
+        }
+    };
+
+    sayNutrition = function () {
+        this.calculateNutrition();
+        console.log(
+            "This " + this.fruitName + " is " + this.howNutritious + " nutritious."
+        );
+    };
 }
 
 let apple = new Fruit("Apple")
 let banan = new Fruit("Banana")
-
-console.log("Our first fruit is a " + apple.name);
-console.log("Our second fruit is a " + banan.name);
+let candy = new Fruit("Candy")
 
 
 apple.speak();
 banan.speak();
+banan.changeName("chicken")
+
+apple.sayNutrition();
+banan.sayNutrition();
+candy.sayNutrition();
