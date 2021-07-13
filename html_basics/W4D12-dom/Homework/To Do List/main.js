11bb65fc9e5fb7fb40ce1ab8d4cbe699dfc297f3
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-07-13 16:56:16 -0500

    todo homework: addItem working with bootstrap styling

--- html_basics/W4D12-dom/Homework/To Do List/main.js
+++ html_basics/W4D12-dom/Homework/To Do List/main.js
@@ -1,3 +1,12 @@
+//Show an unordered list of to do's 
+//Show an input to enter a new to do 
+//Show a button to add a to  do. When the button is clicked: 
+//The text from the input box is used to add a list item to the bottom of the list 
+//The text from the input box is cleared out.
+//When the user clicks on a list item, it is removed
+
+//Extra Credit: - When a list item is clicked, cross it out, then remove it after 1 second.
+
 let todoInput = document.getElementById("todoInput")
 let addButton = document.getElementById("addButton");
 let todoList = document.getElementById("todoList");
@@ -10,10 +19,29 @@ addButton.addEventListener("click", addItem);
 
 
 
-document.getElementById("todoList").outerHTML = `<li>${todoListItems[i]}</li>`;
 
 
 
-function addItem() {
+// function for adding items to the todo list
+function addItem(e, text) {
+    //
+    let newListItem = document.createElement("li");
+    // set input text to the inner text of the <li>
+    newListItem.innerText = text;
+    // add class to new list item
+    newListItem.classList.add("list-group-item")
+    if (!text) {
+        // set inner text of new list item ("li") to the text in input feild
+        newListItem.innerText = todoInput.value;
+        // clear the input box
+        todoInput.value = "";
+    }
+    todoList.appendChild(newListItem);
+
+}
+
+
+// function for removing the todo list items
+function removeItem() {
 
 }
