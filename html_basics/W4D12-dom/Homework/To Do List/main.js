2e9bc974f22681cb94b7c9b03ca12ba72c64ffcc
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-07-17 15:23:27 -0500

    todo homework: added js to add items to list

--- html_basics/W4D12-dom/Homework/To Do List/main.js
+++ html_basics/W4D12-dom/Homework/To Do List/main.js
@@ -7,3 +7,37 @@
 
 //Extra Credit: - When a list item is clicked, cross it out, then remove it after 1 second.
 
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const todoUL = document.getElementById('todos')
+
+// listens for form submit and will not refresh the page
+// uses addTodo function to take text and make new list item
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    addTodo()
+})
+
+function addTodo(todo) {
+    //get text input from the input form 
+    let todoText = input.value
+
+    if (todo) {
+        todoText = todo.text
+    }
+    console.log(todoText);
+    if (todoText) {
+        const todoElement = document.createElement('li')
+        if (todo && todo.completed) {
+            todoElement.classList.add('completed')
+        }
+
+        todoElement.innerText = todoText
+
+        // add input text to dom 
+        todoUL.appendChild(todoElement)
+        // clear the input box after todo element added to list
+        input.value = ''
+    }
+}
