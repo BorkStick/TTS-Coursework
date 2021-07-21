de1772f0b6368eed381b7d3c8c644da33d849638
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-07-21 08:11:20 -0500

    W5D16 Day start

---
+++ java/W5D16-java-data-structures/src/HomeworkHashMap.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+//You are a car dealer - create a hash map of vehicles:
+//
+//The model is the Key, the make is the Value.
+//
+//Ask the customer what car (model) they are looking for,
+//
+//use the HashMap to determine if you have that vehicle, and what make it is.
+//
+//(e.g., "Oh, you're looking for a Civic? Our Honda selection is right over here...")
+public class HomeworkHashMap {
+    public static void main(String[] args) {
+        HashMap<String, Integer> vehicle = new HashMap<>();
+        vehicle.put("");
+    }
+    }
