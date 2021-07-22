f5e140cebe556944e730286b077e617bcba4adf3
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-07-22 11:00:02 -0500

    W5D18 working on homeworks

--- java/W5D16-java-data-structures/src/ArraysHomework.java
+++ java/W5D16-java-data-structures/src/ArraysHomework.java
@@ -1,2 +1,80 @@
+import java.util.Arrays;
+
 public class ArraysHomework {
-}
+    static int[] myNum = {30, 10, 20, 40};
+
+
+    static void sortArray(int [] array) {
+        Arrays.sort(array);
+        System.out.println("We sorted things: " + Arrays.toString(array));
+    }
+
+    // Write a program to sum all the values of a given Array in Java.
+    static void sums(int [] array) {
+        //do math and stuff
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        System.out.println("The sum of " + Arrays.toString(array) + " is " + sum);
+    }
+
+    // 2. What is the output?
+    // java: illegal start of expression
+    // fixed the errors in code and the output is 2
+    static void numberTwo() {
+
+        double[ ] exampleArray = {1,5,6,5,4,1};
+
+        double maximum = exampleArray[0];
+
+        int index = 0;
+
+        for (int i = 1; i<exampleArray.length; i++){
+
+            if (exampleArray[ i ] > maximum) {
+
+                maximum = exampleArray[ i ];
+
+                index = i;
+
+            }
+
+        }
+
+        System.out.println(index);
+    }
+
+    //1. Write a public static method called "toPower" that takes in as parameters two integers called size and power.
+    // The method should return an array of size "size" with each array index raised to the value of "power."
+    // So, for example, if we passed in "size = 4" and "power = 2" to the "toPower," the method should return the following result: [0,1,4,9].
+    static void toPower(int size, int power) {
+        int base = size;
+        int exponent = power;
+        double result = Math.pow(base, exponent);
+        System.out.println("size is: " + size);
+        System.out.println("power is: " + power);
+        System.out.println("result: " + result);
+
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+
+
+        System.out.println("Array numbers are: " + Arrays.toString(myNum));
+        sortArray(myNum);
+        sums(myNum);
+        numberTwo();
+        toPower(4, 2);
+    }
+
+
+
+     }
+
--- java/W5D16-java-data-structures/src/HashMapHomework.java
+++ java/W5D16-java-data-structures/src/HashMapHomework.java
@@ -15,12 +15,15 @@ public class HashMapHomework {
         vehicle.put("Toyota", "Sprinter Trueno AE86");
         vehicle.put("Subaru", "WRX STI Type R");
         vehicle.put("Nissan", "Skyline GT-R");
+        vehicle.put("Nissan", "Silvia (S13)");
+        vehicle.put("Nissan", "180SX/Silvia");
         vehicle.put("Mitsubishi", "Lancer Evo IV");
         vehicle.put("Honda", "Civic Type-R");
+        vehicle.put("Honda", "S2000 (AP1)");
 
 
         System.out.println("Welcome to");
-        System.out.println("==================================");
+        System.out.println("======================================================================================================");
         System.out.println("");
         System.out.println("████████ ██    ██ ██       ██████  ██████  ███████     ██████  ██████  ██ ███████ ████████  ██████ ");
         System.out.println("   ██     ██  ██  ██      ██    ██ ██   ██ ██          ██   ██ ██   ██ ██ ██         ██    ██    ██ ");
@@ -34,19 +37,28 @@ public class HashMapHomework {
         System.out.println("    ██      ██  ██  ██ ██      ██    ██ ██   ██ ██ ██    ██ ██  ██  ██ ");
         System.out.println("    ███████ ██      ██ ██       ██████  ██   ██ ██  ██████  ██      ██ ");
         System.out.println("");
-        System.out.println("==================================");
+        System.out.println("======================================================================================================");
         System.out.println("Take a look at some of my fine wares");
         System.out.println(vehicle);
 
         Scanner userInput = new Scanner(System.in);
         // get user input and search the hashmap
         System.out.println("What kind of vehiclular unit peeks your interest?");
-        String test = userInput.nextLine();
+        String search = userInput.nextLine();
 
-        System.out.printf("I see you are interested in the %s", test);
+        vehicle.containsKey(search);
+        System.out.printf("I see you are interested in the %s \n", search);
+        System.out.println("Lets see if we have any in stock: " + vehicle.values());
 
-        System.out.printf("I aint got no %s's here", test);
+        // check if the search input is in our hashmap
+        if (vehicle.containsKey(search)) {
+            System.out.printf("Looks like we have some %s's in stock. \n", search);
+            System.out.printf("Here are the avalible %s models. \n", search);
+            System.out.println(vehicle.values());
 
+        } else {
+            System.out.printf("I aint got no %s's here", search);
+        }
 
     }
     }
