7560ef050be76d8251bfcdf0df7c3eab005e550d
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-07-27 09:31:09 -0500

    w6d2 class start

---
+++ html_basics/W6D2-POSTMan/main.js
@@ -0,0 +1,53 @@
+// bring in json from external api 
+ fetch('domains.json')
+   .then((response) => response.json())
+   .then((json) => domains = json)
+   .then((json) => console.log(json));
+
+
+
+var domains = [
+  {
+    id: 01,
+    domainName: "google.com",
+    url: "https://google.com",
+    online: true
+  },
+  {
+    id: 02,
+    domainName: "yahoo.com",
+    url: "https://yahoo.com",
+    online: true
+  },
+  {
+    id: 03,
+    domainName: "bork.com",
+    url: "https://bork.com",
+    online: false
+  },
+  {
+    id: 04,
+    domainName: "bork.host",
+    url: "https://bork.host",
+    online: false
+  },
+  {
+    id: 05,
+    domainName: "bork.tech",
+    url: "https://bork.tech",
+    online: false
+  },
+]
+
+//convert to a json string
+domain = JSON.stringify(domains)
+
+
+// log out each domain from the array
+// will also display on the page
+var output = '';
+for(var i = 0;i < domains.length; i++){
+  console.log(domains[i].domainName);
+  output += '<li class="list-group-item">'+ " <b>Domain:</b> "+ domains[i].domainName + "<br>" + " <b>URL:</b> " + '<a href="'+domains[i].url+'">'+domains[i].url+'</a>' + "<br>" + " <b>Online:</b> "+ '<button type="button" class="btn btn-secondary">'+ domains[i].online +'</button>' +'</li>';
+}
+document.getElementById("domains").innerHTML = output;
