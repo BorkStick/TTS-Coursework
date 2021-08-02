968997b9b8ff20fd6d69684788e241fffc3dd5ba
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-08-02 00:17:02 -0500

    working on callback homework

--- html_basics/W6D3-js_callbacks/homework-callbacks/main.js
+++ html_basics/W6D3-js_callbacks/homework-callbacks/main.js
@@ -15,8 +15,60 @@ setTimeout(() => {
 // adding callcack for button click
 window.onload = function () {
     document.querySelector("#callback-btn")
-    // listens for the button click then logs the message to console
+        // listens for the button click then logs the message to console
         .addEventListener("click", function () {
             console.log("User has clicked on the button!");
         });
 }
+
+window.onload = function () {
+    document.querySelector("#status-btn")
+        // listens for the button click then logs the message to console
+        .addEventListener("click", function () {
+            console.log("User has clicked on the status button!");
+            greet('Terry');
+        });
+}
+	
+setInterval(myClock, 1000);
+
+function myClock() {
+    let d = new Date();
+    document.getElementById("clock").innerHTML =
+        d.getHours() + ":" +
+        d.getMinutes() + ":" +
+        d.getSeconds();
+}
+
+let checkThisSite = "https://domain.com"
+let siteStatus = "online"
+document.getElementById("domain").innerHTML = checkThisSite;
+document.getElementById("domainh").innerHTML = checkThisSite;
+document.getElementById("domain-status").innerHTML = siteStatus;
+
+function getDomain(event) {
+    var x = document.getElementById("domain-input").value;
+    document.getElementById("demo").innerHTML = "You selected: " + x;
+   
+  }
+
+// check server status
+
+// fetch from server 
+
+// get response code
+
+// use response codee to determin if offline or online 
+
+
+
+function saySomethingNice() {
+    console.log(`Nice to meet you.`);
+}
+
+function greet(name) {
+    console.log(`Hello ${name}!`);
+    saySomethingNice();
+}
+
+
