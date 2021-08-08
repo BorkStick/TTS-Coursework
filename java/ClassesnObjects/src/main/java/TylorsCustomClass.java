7be092d9cd89f8e1b403e71661f27d1f5aa3d62d
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-08-08 14:31:27 -0500

    Getting this started

---
+++ java/ClassesnObjects/src/main/java/TylorsCustomClass.java
@@ -0,0 +1,11 @@
+// Minimum 3 private instance variables
+// An empty constructor
+// A constructor that uses only 2 of your 3 variables
+// A constructor that uses all 3 of your instance variables
+// Get methods for all 3 of your instance variables and Set methods for all 3 of your instance variables
+// A method that returns a Boolean
+// A method that returns a String (For example, if I created a Santa Custom
+
+public class TylorsCustomClass {
+
+}
