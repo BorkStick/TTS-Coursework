1383326175bfa7b65177303624f9a45ebcc232c8
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-08-08 14:31:52 -0500

    Getting this started

---
+++ java/ClassesnObjects/src/main/java/Pet.java
@@ -0,0 +1,12 @@
+//name (PRIVATE)
+//        age (PRIVATE)
+//        location (PRIVATE)
+//        type (PRIVATE)
+//        two constructors(empty, all attributes)
+//        Code to be able to access the following (Get Methods):
+//        name, age, type
+//        Code to be able to change (Set Methods):
+//        name, age, location
+
+public class Pet {
+}
