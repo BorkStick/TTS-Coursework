f70718eaf746aa9a989924918a947ef56f07db0c
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-08-18 19:18:34 -0500

    react day 2 notes app

--- react/notes-react/src/App.js
+++ react/notes-react/src/App.js
@@ -5,7 +5,7 @@ import Board from './components/Board';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Notes React</h1>
       <Board />
     </div>
   );
